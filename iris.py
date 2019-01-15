5eb939464240bc105de7fb6365d1312386e305cc
Anda Tatomir <anda.tatomir@gmail.com>
2019-01-15 20:56:07 +0200

    Aligned centers

--- iris.py
+++ iris.py
@@ -46,6 +46,7 @@ def main():
 
     not_found_count = 0
 
+    # detect outer_circle
     img_grayscale = cv2.GaussianBlur(img_grayscale, (5,5), 0)
     high_thresh, thresh_im = cv2.threshold(img_grayscale, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     low_thresh = 0.3 * high_thresh
@@ -58,43 +59,55 @@ def main():
     outer_circles = cv2.HoughCircles(canny, cv2.HOUGH_GRADIENT, 2, 50, param1=30, param2=80, minRadius=0,
                                      maxRadius=int(rows/2))
     if outer_circles is not None:
-        if outer_circles[0].shape[0] > 1:
-            print("found more!")
-            outer_circles[0] = sorted(outer_circles[0], key=lambda circle: euclidean_dist(circle[0], rows/2, circle[1], cols/2))
-            outer_circles[0] = outer_circles[0][:1]
-        img_color = print_circles(outer_circles[0], img_color)
+        outer_circles[0] = sorted(outer_circles[0], key=lambda circle: euclidean_dist(circle[0], rows/2, circle[1], cols/2))
+        iris_circle = outer_circles[0][0]
+        # img_color = print_circles([iris_circle], img_color)
+
+        # detect pupil circle
+        iris = outer_circles[0][0]
+        iris_radius = iris[2]
+        roi = img_grayscale[int(iris[1] - iris_radius): int(iris[1] + iris_radius),
+              int(iris[0] - iris_radius): int(iris[0] + iris_radius)]
+        cv2.imshow("roi", roi)
+
+        kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+        sharpened = cv2.filter2D(roi, -1, kernel)
+        cv2.imshow("sharpened", sharpened)
+        pupil_thresh = 50
+        _, th = cv2.threshold(sharpened, pupil_thresh, 255, cv2.THRESH_BINARY_INV)
+        cv2.imshow("thresholded", th)
+        # th = cv2.morphologyEx(th, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3)))
+        # cv2.imshow("pupil closed", th)
+        # th = cv2.morphologyEx(th, cv2.MORPH_CLOSE, np.ones((2, 2), np.uint8), 2)
+        # cv2.imshow("opened", th)
+
+        inner_circles = cv2.HoughCircles(th, cv2.HOUGH_GRADIENT, 2, 10, param1=30, param2=40, minRadius=0,
+                                         maxRadius=int(0.8 * iris_radius))
+        if inner_circles is not None:
+            # average_radius = zip(*inner_circles[0])
+            # print (average_radius)
+            inner_circles[0] = sorted(inner_circles[0],
+                                      key=lambda circle: euclidean_dist(circle[0], iris[0], circle[1], iris[1]))
+            pupil_circle = inner_circles[0][0]
+            print(pupil_circle)
+            # translate back in the big image coord
+            pupil_circle[0] += int(iris[0] - iris_radius)
+            pupil_circle[1] += int(iris[1] - iris_radius)
+            #img_color = print_circles([pupil_circle], img_color)
+            # img_color = print_circles(inner_circles[0], img_color)
+
+        # align_circles
+        pupil_circle[0] = int(0.5 *(iris_circle[0] + pupil_circle[0]))
+        iris_circle[0] = pupil_circle[0]
+        pupil_circle[1] = int(0.5 * (iris_circle[1] + pupil_circle[1]))
+        iris_circle[1] = pupil_circle[1]
+
+        aligned = print_circles([pupil_circle, iris_circle], img_color)
+
+        #normalize
+
     else:
         not_found_count += 1
-
-    #detect pupil circle
-    iris = outer_circles[0][0]
-    iris_radius = iris[2]
-    roi = img_grayscale[int(iris[1] - iris_radius): int(iris[1] + iris_radius),
-          int(iris[0] - iris_radius): int(iris[0] + iris_radius)]
-    cv2.imshow("roi", roi)
-
-    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
-    sharpened = cv2.filter2D(roi, -1, kernel)
-    cv2.imshow("sharpened", sharpened)
-    pupil_thresh = 50
-    _, th = cv2.threshold(sharpened, pupil_thresh, 255, cv2.THRESH_BINARY_INV)
-    cv2.imshow("thresholded", th)
-    # opened = cv2.morphologyEx(th, cv2.MORPH_OPEN, np.ones((2, 2), np.uint8), 3)
-    # cv2.imshow("opened", opened)
-
-    inner_circles = cv2.HoughCircles(th, cv2.HOUGH_GRADIENT, 2, 10, param1=30, param2=50, minRadius=0,
-                                     maxRadius=int(0.75 * iris_radius))
-    if inner_circles is not None:
-        average_radius = zip(*inner_circles[0])
-        print (average_radius)
-        # inner_circles[0] = sorted(inner_circles[0],
-        #                           key=lambda circle: euclidean_dist(circle[0], iris[0], circle[1], iris[1]))
-        # inner_circles[0] = inner_circles[0][:1]
-        pupil_img = print_circles(inner_circles[0], cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR))
-        cv2.imshow("pupil", pupil_img)
-
-
-
 
     cv2.imshow("outer circle", img_color)
     cv2.imshow("thresh", thresh_im)
